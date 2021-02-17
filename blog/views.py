819d8684c5684ca9acd93fcc9a358b2427c9a09c
ayush1409 <ayushsingh45556@gmail.com>
2021-02-17 12:31:55 +0530

    ran Post and User migrations

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-# dummy posts
-posts = [
-	{
-		'author': 'ayush',
-		'title': 'Blog Post1',
-		'content': 'Awesome',
-		'date_posted' : '15-02-2021'
-	},
-	{
-		'author': 'Tony',
-		'title': 'Blog Post2',
-		'content': 'Geek',
-		'date_posted' : '15-02-2021'
-	}
-]
 # for handling the home page requests
 def home(request):
 	context = {
-		'posts' : posts
+		'posts' : Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
